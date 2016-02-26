74b4273906a50c2d506e1840c2191f63560a16f2
Alexander Biryukov <aleksbiryukov@gmail.com>
2016-02-27 03:36:05 +0600

    before removing buttons

--- SelfieFood.App/App.xaml.cs
+++ SelfieFood.App/App.xaml.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
-namespace camera_Temp
+namespace SelfieFood.App
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- SelfieFood.App/MainPage.xaml.cs
+++ SelfieFood.App/MainPage.xaml.cs
@@ -1,28 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Xml.Linq;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+//For MediaCapture
+//For Encoding Image in JPEG format
+//For storing Capture Image in App storage or in Picture Library
 
-using Windows.Media.Capture;            //For MediaCapture
-using Windows.Media.MediaProperties;    //For Encoding Image in JPEG format
-using Windows.Storage;                  //For storing Capture Image in App storage or in Picture Library
-using Windows.UI.Xaml.Media.Imaging;    //For BitmapImage. for showing image on screen we need BitmapImage format.
+//For BitmapImage. for showing image on screen we need BitmapImage format.
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
-namespace camera_Temp
+namespace SelfieFood.App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
