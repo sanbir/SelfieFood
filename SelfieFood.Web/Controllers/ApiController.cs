3786f1872fe3bd1c13447884840c53ca810aa0b7
Yury Baranikhin <BaranikhinYury@gmail.com>
2016-02-27 03:59:40 +0600

    Получение эмоций и лиц с возрастом

--- SelfieFood.Web/Controllers/ApiController.cs
+++ SelfieFood.Web/Controllers/ApiController.cs
@@ -9,11 +9,39 @@ using SelfieFood.Common;
 
 namespace SelfieFood.Web.Controllers
 {
+    using System.IO;
+
+    using Microsoft.ProjectOxford.Emotion;
+    using Microsoft.ProjectOxford.Emotion.Contract;
+    using Microsoft.ProjectOxford.Face;
+    using Microsoft.ProjectOxford.Face.Contract;
+
     public class FoodApiController : ApiController
     {
         public ResturantsResponse PostPhoto()
         {
             return null;
+        }
+
+        private Face[] GetFaces(Byte[] image)
+        {
+            FaceServiceClient faceServiceClient = new FaceServiceClient("53ea3efc28f34a549049dc91e03bac69");
+            using (var imageStream = new MemoryStream(image))
+            {
+                return
+                    faceServiceClient.DetectAsync(
+                        imageStream,
+                        returnFaceAttributes: new FaceAttributeType[] { FaceAttributeType.Age, }).Result;
+            }
+        }
+
+        private Emotion[] GetEmotions(Byte[] image)
+        {
+            EmotionServiceClient client = new EmotionServiceClient("cb68f67d5c47415ead0fd289b051acc6");
+            using (var imageStream = new MemoryStream(image))
+            {
+                return client.RecognizeAsync(imageStream).Result;
+            }
         }
     }
 }
