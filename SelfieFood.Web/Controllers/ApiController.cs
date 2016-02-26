43e6a763bccbc1cb20a946b7a40280cba979abcd
Yuiry Vasilyev <yuriy@y-vasilyev.ru>
2016-02-27 00:18:14 +0600

    добавлены сборки

---
+++ SelfieFood.Web/Controllers/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SelfieFood.Common
+
+namespace SelfieFood.Web.Controllers
+{
+    public class FoodApiController : ApiController
+    {
+        public RestrauntResult PostPhoto()
+        {
+
+        }
+
+    }
+}
