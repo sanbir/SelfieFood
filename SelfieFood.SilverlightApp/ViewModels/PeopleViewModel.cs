28791955600ce567a918438f5124827a0101021d
Alexander Biryukov <aleksbiryukov@gmail.com>
2016-02-27 21:30:25 +0600

    add people view models

--- SelfieFood.SilverlightApp/ViewModels/PeopleViewModel.cs
+++ SelfieFood.SilverlightApp/ViewModels/PeopleViewModel.cs
@@ -1,14 +1,49 @@
-﻿namespace SelfieFood.SilverlightApp.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelfieFood.SilverlightApp.ViewModels
 {
-    class PeopleViewModel
+    public class PeopleViewModel
     {
-        public int Infants { get; set; }
-        public int Children { get; set; }
-        public int Men { get; set; }
-        public int BeardedMen { get; set; }
-        public int Women { get; set; }
-        public int OldMen { get; set; }
+        public List<PersonViewModel> PeopleViewModels { get; set; }
+
+        public string AgeDiapason
+        {
+            get
+            {
+                return PeopleViewModels == null
+                    ? ""
+                    : PeopleViewModels.Count == 0
+                        ? ""
+                        : PeopleViewModels.Count == 1
+                            ? PeopleViewModels[0].Age.ToString()
+                            : GetAgeDiapason();
+            }
+        }
+
+        private string GetAgeDiapason()
+        {
+            int min = PeopleViewModels.Select(p=>p.Age).Min();
+            int max = PeopleViewModels.Select(p => p.Age).Max();
+            return string.Format("{0} - {1}", min, max);
+        }
     }
 
+    public class PersonViewModel
+    {
+        public PersonType PersonType { get; set; }
 
+        public int Age { get; set; }
+    }
+
+    public enum PersonType
+    {
+        Infant,
+        Child,
+        Man,
+        BeardedMan,
+        Woman,
+        OldMan
+    }
 }
